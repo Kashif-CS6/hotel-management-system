9d3dacf3ba60f1acbdac3ea832bb3ac01f5ce07c
Kashif-CS6 <kashifnabeel.cs@gmail.com>
2025-10-31 01:19:01 +0500

    changed stepper file

--- src/app/book-hotel/[slug]/page.jsx
+++ src/app/book-hotel/[slug]/page.jsx
@@ -1,12 +1,5 @@
 "use client";
-
 import Stepper from "@/components/bookHotel/Stepper";
-import Confirmation from "@/components/bookHotel/confirmation";
-
-import { useForm } from "react-hook-form";
-
-import { useState } from "react";
-import Parkingform from "@/components/bookHotel/parkingform";
 
 const page = () => {
   return (
